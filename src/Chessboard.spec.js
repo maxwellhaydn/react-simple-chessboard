be3c72e9aa5a71dde8a31e2ed2eb72892037e554
Max Carey <mh.carey@pm.me>
2020-07-06 08:23:06 -0500

    Add license notices to source files

--- src/Chessboard.spec.js
+++ src/Chessboard.spec.js
@@ -1,3 +1,23 @@
+/**
+ * Define the `Chessboard` React component
+ *
+ * Copyright © 2020 Maxwell Carey
+ *
+ * This file is part of react-simple-chessboard.
+ *
+ * react-simple-chessboard is free software: you can redistribute it
+ * and/or modify it under the terms of the GNU General Public License
+ * version 3, as published by the Free Software Foundation.
+ *
+ * react-simple-chessboard is distributed in the hope that it will be
+ * useful, but WITHOUT ANY WARRANTY; without even the implied warranty
+ * of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
 import React from 'react';
 import { mount } from 'enzyme';
 import { expect } from 'chai';
